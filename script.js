7a626b3c5db8d12ad1e2efb839308b079a9f3a97
facundo alvarez <fakualvarez1998@gmail.com>
2022-05-18 19:43:03 -0300

    desafio 1

---
+++ script.js
@@ -0,0 +1,29 @@
+
+alert("Hola soy tu agente de censo, hoy voy a tomar unos pequeños datos");
+
+let i=0;
+let Pasar="";
+let Nombre=0;
+
+while(i==0){  
+    Pasar = prompt("si esta de acuerdo escriba si \n sino estas deacuerdo escriba no"); 
+
+    if((Pasar=="si") || (Pasar=="Si") || (Pasar=="SI") || (Pasar=="yes")){
+            i=1;    
+    }else{
+        alert("hasta que no ponga que si no se va a poder ir :)")
+          };
+};
+if(i!=0){
+  alert("veo que decidio seguir con el censo, muchas gracias :)");
+  i=0;
+}
+while(i==0){
+  Nombre=prompt("escriba su nombre y apellido por favor")
+  if(Nombre==0){
+    i++
+  }
+
+}
+
+
